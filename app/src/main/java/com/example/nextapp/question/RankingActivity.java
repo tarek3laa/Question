b8dfe1ec096694f09c91ba3a6cf78a2156680161
tarek3laa <tarek_3laa@hotmail.com>
2018-08-18 16:00:43 +0200

    tarek edit2

--- app/src/main/java/com/example/nextapp/question/RankingActivity.java
+++ app/src/main/java/com/example/nextapp/question/RankingActivity.java
@@ -2,12 +2,18 @@ package com.example.nextapp.question;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class RankingActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rankactivity);
+        setContentView(R.layout.activity_ranking);
+
+       listView=(ListView)findViewById(R.id.lv_ranking);
+
     }
 }

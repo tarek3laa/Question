e697d448b644ea9fa6b9a14dbc99aef62b3e53ae
tarek3laa <tarek_3laa@hotmail.com>
2018-08-23 22:18:16 +0200

    edit

--- app/src/main/java/com/example/nextapp/question/MainActivity.java
+++ app/src/main/java/com/example/nextapp/question/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.nextapp.question;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.text.Html;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toolbar;
 
 import com.example.nextapp.question.Data.DataBase;
 import com.example.nextapp.question.Data.User;
@@ -17,15 +22,17 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 public class MainActivity extends AppCompatActivity {
 
 
-
    private CardView mProfile;
    private LinearLayout sport;
    public boolean isSignUp =false;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle(Html.fromHtml("<font  color='#000000' > Question</font>"));
 
         mProfile=(CardView)findViewById(R.id.cv_profile);
 
--- app/src/main/java/com/example/nextapp/question/QuestionActivity.java
+++ app/src/main/java/com/example/nextapp/question/QuestionActivity.java
@@ -1,13 +1,16 @@
 package com.example.nextapp.question;
 
 import android.os.CountDownTimer;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.TextView;
 
 import java.util.Locale;
 
 public class QuestionActivity extends AppCompatActivity {
+
 
     private static final long START_TIME = 15000;
     private long mTime = START_TIME;
@@ -19,8 +22,10 @@ public class QuestionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
         timer = (TextView) findViewById(R.id.tv_timer);
-
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle(Html.fromHtml("<font  color='#000000' > Sport</font>"));
         startTimer();
+
 
     }
 

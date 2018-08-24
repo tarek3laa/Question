5fc114b602d1d87fd62881bd56e80ff55fe38421
tarek3laa <tarek_3laa@hotmail.com>
2018-08-24 20:58:35 +0200

    edit11

--- app/src/main/java/com/example/nextapp/question/Data/DataBase.java
+++ app/src/main/java/com/example/nextapp/question/Data/DataBase.java
@@ -1,20 +1,32 @@
 package com.example.nextapp.question.Data;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.reflect.Array;
 
 import static java.lang.Integer.parseInt;
 
 public class DataBase {
 
     DocumentReference reference;
+    CollectionReference reference1;
+
+
 
     public void setData(String name,String userName){
-        reference= FirebaseFirestore.getInstance().document("sample/"+userName);
+        reference= FirebaseFirestore.getInstance().document("sample");
+
+
+
+
         User user1 = null;
         user1=new User(name,userName,1,1,1);
 
         reference.set(user1);
+
 
 
 

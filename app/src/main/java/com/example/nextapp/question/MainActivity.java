92175291c5d60cbf1004a8e95bdd978488e8d033
tarek3laa <tarek_3laa@hotmail.com>
2018-08-24 15:04:42 +0200

    edit10

--- app/src/main/java/com/example/nextapp/question/MainActivity.java
+++ app/src/main/java/com/example/nextapp/question/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.nextapp.question;
 
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,13 +9,7 @@ import android.support.v7.widget.CardView;
 import android.text.Html;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.Toolbar;
 
-import com.example.nextapp.question.Data.DataBase;
-import com.example.nextapp.question.Data.User;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,14 +18,12 @@ public class MainActivity extends AppCompatActivity {
    private CardView mProfile;
    private LinearLayout sport;
    public boolean isSignUp =false;
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ActionBar actionBar=getSupportActionBar();
-        actionBar.setTitle(Html.fromHtml("<font  color='#000000' > Question</font>"));
 
         mProfile=(CardView)findViewById(R.id.cv_profile);
 
